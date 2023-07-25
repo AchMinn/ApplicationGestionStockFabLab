ac0fd77f00ad5dabd158812071da5c864c82860e
AchMi <92281310+AchMinn@users.noreply.github.com>
2023-07-25 11:32:19 +0100

    Update index.php

--- index.php
+++ index.php
@@ -25,54 +25,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Main Page</title>
-	<link rel="stylesheet" href="index.css">
-	<link rel="stylesheet" href="index.js">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Main Page</title>
+  <link rel="stylesheet" href="index.css">
+  <link rel="stylesheet" href="index.js">
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"/>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 </head>
 <body>
-      <div id="headerWrapper">
-<header role="banner">
-  <div class="header__avatar"><a href="index.php" style="text-decoration: none;"><img src="universialogo.png"></a></div>
-  <div id="login">
-  <div id="form">
-    <h2>Connecter vous</h2>
-    <form action="index.php" method="POST">
-    <input type="text" placeholder="Username" name="username">
-    <input type="password" placeholder="Password" name="password">
-    <input type="submit" value="Log in" class="submit" name="sumbit">
-    <input type="button" value="Close" class="close" onclick="closeForm()">
-    </form>
-  </div>
-</div>
-</header>
-
-<nav role="navigation">
-  <ul class="menu__list">
-    <li class="menu__item">
-      <a href="Operateur.php" style="text-decoration: none;"><button class="menu__link" >Operateur</button></a>
-    </li>
-    <li class="menu__item">
-     <a href="Machines.php" style="text-decoration: none;"><button class="menu__link">Machines</button></a>
-    </li>
-    <li class="menu__item">
-     <a href="Consommateur.php" style="text-decoration: none;"><button class="menu__link">Consommateur</button></a>
-    </li>
-    <li class="menu__item">
-     <a href="Fournisseur.php" style="text-decoration: none;"><button class="menu__link">Fournisseur</button></a>
-    </li>
-    <li class="menu__item">
-     <a href="Filaments.php" style="text-decoration: none;"><button class="menu__link">Filaments</button></a>
-    </li>
-    <li class="menu__item">
-     <button class="menu__link" onclick="openForm()">Se Connecter</button>
-    </li>
-  </ul>
-</nav>
-        </div>
+      <?php include("header.php") ?>
 <div id="bodyWrapper">
   <div class="mx-auto w-full md:w-4/5 px-4">
         <div class="container my-8">
