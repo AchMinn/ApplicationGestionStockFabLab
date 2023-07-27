355170f25916274899d64049b9c643a1c0763cf6
AchMi <92281310+AchMinn@users.noreply.github.com>
2023-07-27 12:03:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php 
 
-include("config.php");
 session_start();
-// var_dump($_POST);
+if(isset($_SESSION['username']))
+ {
+    header("Location:loggedinIndex.php"); 
+ }
+include("config.php");
 if($_SERVER["REQUEST_METHOD"] == "POST"){
   $username = mysqli_real_escape_string($db,$_POST['username']);
   $password = mysqli_real_escape_string($db,$_POST['password']);
@@ -14,13 +17,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
   $count = mysqli_num_rows($result);
 
    if($count == 1) {
-         // session_register("myusername");
-         $_SESSION['myusername'] = $username;
-         header("location: Operateur.php");
-         die();
-      }
+         $_SESSION['username'] = $username;
+         header("location:loggedinIndex.php");       
+       }
+    else{
+      echo "<script type='text/javascript'>alert('Invalid Username or Password');</script>";
+    }
    }
-
  ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -35,6 +38,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <body>
       <?php include("header.php") ?>
 <div id="bodyWrapper">
+      <?php 
+       ?>
   <div class="mx-auto w-full md:w-4/5 px-4">
         <div class="container my-8">
           <div class="flex justify-between items-center mb-4">
@@ -46,20 +51,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <div class="flex-none w-2/3 md:w-1/3 mr-8 md:pb-4 border rounded-lg">
               <a href="#" class="space-y-4">
                 <div class="aspect-w-16 aspect-h-9">
-                  <img class="object-cover shadow-md hover:shadow-xl rounded-lg" src="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixqx=3H1AJd0Pae&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt=""
+                  <img class="object-cover shadow-md hover:shadow-xl rounded-lg" src="Raise3D.jpg" alt="Not Found"
                   />
                 </div>
                 <div class="px-4 py-2">
                   <div class="text-lg leading-6 font-medium space-y-1">
                     <h3 class="font-bold text-gray-800 text-3xl mb-2">
-                      Some title goes here
+                      Raise 3D E2CF
                     </h3>
                   </div>
                   <div class="text-lg">
                     <p class="">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Ad recusandae, consequatur corrupti vel quisquam id itaque
-                      nam
+                      est une imprimante 3D IDEX optimisée pour l'impression 3D de filaments renforcés de fibre de carbone. 
                     </p>
                   </div>
                 </div>
@@ -68,20 +71,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <div class="flex-none w-2/3 md:w-1/3 mr-8 md:pb-4 border rounded-lg">
               <a href="#" class="space-y-4">
                 <div class="aspect-w-16 aspect-h-9">
-                  <img class="object-cover shadow-md hover:shadow-xl rounded-lg" src="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixqx=3H1AJd0Pae&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt=""
+                  <img class="object-cover shadow-md hover:shadow-xl rounded-lg" src="TechnoDrill3.jpg" alt=""
                   />
                 </div>
                 <div class="px-4 py-2">
                   <div class="text-lg leading-6 font-medium space-y-1">
                     <h3 class="font-bold text-gray-800 text-3xl mb-2">
-                      Some title goes here
+                      TechnoDrill 3
                     </h3>
                   </div>
                   <div class="text-lg">
                     <p class="">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Ad recusandae, consequatur corrupti vel quisquam id itaque
-                      nam
+                      une machine de prototypage rapide 3D. Il peut percer, couper, distribuer du plastique, du métal et des PCB avec une grande précision, et également graver la face avant de l'aluminium ou du plastique.
                     </p>
                   </div>
                 </div>
@@ -90,20 +91,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <div class="flex-none w-2/3 md:w-1/3 mr-8 md:pb-4 border rounded-lg">
               <a href="#" class="space-y-4">
                 <div class="aspect-w-16 aspect-h-9">
-                  <img class="object-cover shadow-md hover:shadow-xl rounded-lg" src="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixqx=3H1AJd0Pae&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt=""
+                  <img class="object-cover shadow-md hover:shadow-xl rounded-lg" src="CIF.jpg" alt=""
                   />
                 </div>
                 <div class="px-4 py-2">
                   <div class="text-lg leading-6 font-medium space-y-1">
                     <h3 class="font-bold text-gray-800 text-3xl mb-2">
-                      Some title goes here
+                      TechnoDrill 2
                     </h3>
                   </div>
                   <div class="text-lg">
                     <p class="">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Ad recusandae, consequatur corrupti vel quisquam id itaque
-                      nam
+                      elle grave, perce et détoure des pièces plastique, métal, l'aluminium et les circuits imprimés.,Telle une machine à commande numérique, elle façonne les pièces quelle que soit la source du dessin.
                     </p>
                   </div>
                 </div>
@@ -112,117 +111,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <div class="flex-none w-2/3 md:w-1/3 mr-8 md:pb-4 border rounded-lg">
               <a href="#" class="space-y-4">
                 <div class="aspect-w-16 aspect-h-9">
-                  <img class="object-cover shadow-md hover:shadow-xl rounded-lg" src="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixqx=3H1AJd0Pae&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt=""
+                  <img class="object-cover shadow-md hover:shadow-xl rounded-lg" src="Imprim3dInterne.jpg" alt=""
                   />
                 </div>
                 <div class="px-4 py-2">
                   <div class="text-lg leading-6 font-medium space-y-1">
                     <h3 class="font-bold text-gray-800 text-3xl mb-2">
-                      Some title goes here
+                      La Stratasys Dimension 1200es 
                     </h3>
                   </div>
                   <div class="text-lg">
                     <p class="">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Ad recusandae, consequatur corrupti vel quisquam id itaque
-                      nam
-                    </p>
-                  </div>
-                </div>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="container my-8">
-          <div class="flex justify-between items-center mb-4">
-            <h2 class="text-3xl font-medium">
-              Les machines de FabLab
-            </h2>
-          </div>
-          <div id="scrollContainer" class="flex flex-no-wrap overflow-x-scroll scrolling-touch items-start mb-8">
-            <div class="flex-none w-2/3 md:w-1/3 mr-8 md:pb-4 border rounded-lg">
-              <a href="#" class="space-y-4">
-                <div class="aspect-w-16 aspect-h-9">
-                  <img class="object-cover shadow-md hover:shadow-xl rounded-lg" src="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixqx=3H1AJd0Pae&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt=""
-                  />
-                </div>
-                <div class="px-4 py-2">
-                  <div class="text-lg leading-6 font-medium space-y-1">
-                    <h3 class="font-bold text-gray-800 text-3xl mb-2">
-                      Some title goes here
-                    </h3>
-                  </div>
-                  <div class="text-lg">
-                    <p class="">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Ad recusandae, consequatur corrupti vel quisquam id itaque
-                      nam
-                    </p>
-                  </div>
-                </div>
-              </a>
-            </div>
-            <div class="flex-none w-2/3 md:w-1/3 mr-8 md:pb-4 border rounded-lg">
-              <a href="#" class="space-y-4">
-                <div class="aspect-w-16 aspect-h-9">
-                  <img class="object-cover shadow-md hover:shadow-xl rounded-lg" src="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixqx=3H1AJd0Pae&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt=""
-                  />
-                </div>
-                <div class="px-4 py-2">
-                  <div class="text-lg leading-6 font-medium space-y-1">
-                    <h3 class="font-bold text-gray-800 text-3xl mb-2">
-                      Some title goes here
-                    </h3>
-                  </div>
-                  <div class="text-lg">
-                    <p class="">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Ad recusandae, consequatur corrupti vel quisquam id itaque
-                      nam
-                    </p>
-                  </div>
-                </div>
-              </a>
-            </div>
-            <div class="flex-none w-2/3 md:w-1/3 mr-8 md:pb-4 border rounded-lg">
-              <a href="#" class="space-y-4">
-                <div class="aspect-w-16 aspect-h-9">
-                  <img class="object-cover shadow-md hover:shadow-xl rounded-lg" src="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixqx=3H1AJd0Pae&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt=""
-                  />
-                </div>
-                <div class="px-4 py-2">
-                  <div class="text-lg leading-6 font-medium space-y-1">
-                    <h3 class="font-bold text-gray-800 text-3xl mb-2">
-                      Some title goes here
-                    </h3>
-                  </div>
-                  <div class="text-lg">
-                    <p class="">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Ad recusandae, consequatur corrupti vel quisquam id itaque
-                      nam
-                    </p>
-                  </div>
-                </div>
-              </a>
-            </div>
-            <div class="flex-none w-2/3 md:w-1/3 mr-8 md:pb-4 border rounded-lg">
-              <a href="#" class="space-y-4">
-                <div class="aspect-w-16 aspect-h-9">
-                  <img class="object-cover shadow-md hover:shadow-xl rounded-lg" src="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixqx=3H1AJd0Pae&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt=""
-                  />
-                </div>
-                <div class="px-4 py-2">
-                  <div class="text-lg leading-6 font-medium space-y-1">
-                    <h3 class="font-bold text-gray-800 text-3xl mb-2">
-                      Some title goes here
-                    </h3>
-                  </div>
-                  <div class="text-lg">
-                    <p class="">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Ad recusandae, consequatur corrupti vel quisquam id itaque
-                      nam
+                      une imprimante 3D professionnelle qui utilise la technologie Fused Deposition Modeling (FDM) pour produire des modèles 3D en thermoplastiques ABS.
                     </p>
                   </div>
                 </div>
